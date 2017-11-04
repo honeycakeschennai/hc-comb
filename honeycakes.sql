9642a06faaa1fdb83a985bf6bcc6d63cefb83f8f
Ramu Ramasamy <rram.ramasamy@gmail.com>
2017-11-04 22:51:31 +0530

    SQL commit
    vendor_items_view added
    removed vendor_code column in items table

--- honeycakes.sql
+++ honeycakes.sql
@@ -3,11 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
-<<<<<<< HEAD
--- Generation Time: Oct 23, 2017 at 06:53 PM
-=======
--- Generation Time: Oct 25, 2017 at 04:35 AM
->>>>>>> order
+-- Generation Time: Nov 04, 2017 at 06:14 PM
 -- Server version: 5.6.35
 -- PHP Version: 7.1.1
 
@@ -27,39 +23,31 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
-<<<<<<< HEAD
--- Table structure for table `cakes`
+-- Table structure for table `items`
 --
 
-CREATE TABLE `cakes` (
-  `cake_id` int(11) NOT NULL,
-  `cake_name` varchar(100) NOT NULL,
-  `cake_status` varchar(50) NOT NULL,
-  `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `cakes`
---
-
-INSERT INTO `cakes` (`cake_id`, `cake_name`, `cake_status`, `created_time`) VALUES
-(1, 'Black Forest', 'READY', '2017-08-31 15:34:14'),
-(2, 'White Forest', 'READY', '2017-08-31 15:34:14');
-
-=======
--- Table structure for table `Items`
---
-
-CREATE TABLE `Items` (
+CREATE TABLE `items` (
   `item_id` int(11) NOT NULL,
   `item_code` varchar(10) NOT NULL,
   `item_name` varchar(50) NOT NULL,
   `item_category` varchar(50) NOT NULL,
   `item_price` varchar(10) NOT NULL,
-  `vendor_code` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `qty_slab` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
->>>>>>> order
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`item_id`, `item_code`, `item_name`, `item_category`, `item_price`, `qty_slab`) VALUES
+(1, 'IT001', 'Blackforest', '1', '850', 1),
+(2, 'IT002', 'Whiteforest', '1', '850', 1),
+(3, 'IT003', 'Chocotruffle', '1', '700', 1),
+(4, 'IT004', 'Jelly Cake', '1', '900', 1),
+(5, 'IT005', 'Strawberry', '1', '750', 1),
+(6, 'IT006', 'Butterscotch', '1', '750', 1),
+(7, 'IT007', 'Vanilla', '1', '700', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -78,13 +66,16 @@ CREATE TABLE `locations` (
 
 INSERT INTO `locations` (`location_id`, `location_code`, `location_name`) VALUES
 (1, 'LN001', 'Thiruvanmiyur'),
-(2, 'LN002', 'Perungudi');
+(2, 'LN002', 'Perungudi'),
+(3, 'LN003', 'Thoraipakkam'),
+(4, 'LN004', 'Karapakkam'),
+(5, 'LN005', 'Sholinganallur'),
+(6, 'LN006', 'Chemmancherry'),
+(7, 'LN007', 'Navallur');
 
 -- --------------------------------------------------------
 
 --
-<<<<<<< HEAD
-=======
 -- Table structure for table `orders`
 --
 
@@ -129,7 +120,6 @@ CREATE TABLE `order_items` (
 -- --------------------------------------------------------
 
 --
->>>>>>> order
 -- Table structure for table `tokens`
 --
 
@@ -140,41 +130,6 @@ CREATE TABLE `tokens` (
   `token` varchar(200) NOT NULL,
   `valid_till` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `tokens`
---
-
-INSERT INTO `tokens` (`token_id`, `user_id`, `generated_time`, `token`, `valid_till`) VALUES
-(1, 1, '2017-08-30 22:10:10', 'OTc0YTY3NzdkNTg1ODJhZmRiYWRkYWVkNzMyMjhkMjk=MGQ4ZjQyOTMwZDFmMTYyOTdlOTQ2MDZmODhlMmU2ZGI=NGU2NTAzZmExNWQ3MDUwZDJmODYzNDI4M2JiZmY3MWY=', '0000-00-00 00:00:00'),
-(2, 1, '2017-08-30 22:13:11', 'YTFhZTMyZTNiMDNiNDk5ZmJhNDg1ODNmZTY2YWUxY2M=MGQ4ZjQyOTMwZDFmMTYyOTdlOTQ2MDZmODhlMmU2ZGI=YzczZjQ2YmVmY2UxYjY2ZGEyNzdkNjBkMTAzNmNiM2M=', '0000-00-00 00:00:00'),
-(3, 1, '2017-08-30 22:17:10', 'M2FlMTE2ZjA3NThjYThkYTA2NzY4NTc0YThhZmU1ZmM=MGQ4ZjQyOTMwZDFmMTYyOTdlOTQ2MDZmODhlMmU2ZGI=OWZhYzg0ZGIzMjM3ODViNjZkNjA5NTlkYWQ4OGZiMjY=', '0000-00-00 00:00:00'),
-(4, 1, '2017-08-30 22:17:16', 'NGRjMzdmOGEzODYxNDY3YmYzNGQyMzVlMWZkYzU5ZjQ=MGQ4ZjQyOTMwZDFmMTYyOTdlOTQ2MDZmODhlMmU2ZGI=NjcyMGEwYTA4MDEwOWRiZmI0MTAyOTkyMDA3ZWMyNzM=', '0000-00-00 00:00:00'),
-(5, 1, '2017-08-30 22:17:17', 'ZTI1YTQ5Y2UzMWYyODNlNzA4NmY2ZDdkZmNhNGRiYTY=MGQ4ZjQyOTMwZDFmMTYyOTdlOTQ2MDZmODhlMmU2ZGI=OGJjZTJkMjI2NDQ5M2UxZGUwODIxMjIyYmNkYmYwZjA=', '0000-00-00 00:00:00'),
-(6, 1, '2017-08-30 22:17:18', 'Y2FiYzdjZWI3MjBlOGI0NTg2YjY3Y2M4NTFjZjJhOTM=MGQ4ZjQyOTMwZDFmMTYyOTdlOTQ2MDZmODhlMmU2ZGI=NWVlYTNlMDMzMThhNTYzYjI0M2FiOGYyNjA1MGEyNjY=', '0000-00-00 00:00:00'),
-(7, 1, '2017-08-30 22:17:19', 'MTMxNGEyNzJhYWE0OWY2MjNjY2MzY2IxMmQ1MTM1Mzk=MGQ4ZjQyOTMwZDFmMTYyOTdlOTQ2MDZmODhlMmU2ZGI=OWM4NmRiZTAxMmQwNDRmMDAyY2Y4MGZjYzE3MzRiNjM=', '0000-00-00 00:00:00'),
-(8, 1, '2017-08-30 22:56:07', 'NzFiM2RiZmQwYTQ2NzJkOTZiMDBlZmNhYzlmMzExNjc=MGQ4ZjQyOTMwZDFmMTYyOTdlOTQ2MDZmODhlMmU2ZGI=YjYwYjZmZTNkMWZkY2I5MzZjNDI2MDJmOTEyMjNhY2Y=', '0000-00-00 00:00:00'),
-(9, 1, '2017-08-30 22:56:19', 'MjAyMjQyOWRiMTE5MTFlMTNiMTExYzY3MzQ4YWQ0Mzc=MGQ4ZjQyOTMwZDFmMTYyOTdlOTQ2MDZmODhlMmU2ZGI=NzFjOWFmYmMyZjljY2JjOGVkNjI3NWJiNTc4ZWFmNDA=', '0000-00-00 00:00:00'),
-(10, 1, '2017-08-30 22:56:20', 'MzYxY2M1YTA5NTZiOTUzZTZmNjNhMzRhYzU4ZDg5NDY=MGQ4ZjQyOTMwZDFmMTYyOTdlOTQ2MDZmODhlMmU2ZGI=NTU1Y2E2YTMwMzA2ZjNhODY3ZWZlNGIwZDUzMmVkNDM=', '0000-00-00 00:00:00'),
-(11, 1, '2017-08-30 22:56:21', 'YTk3ZDdlMTBhMzk2NTUwYmJiNTc2NGQxMDMzMzA0N2Q=MGQ4ZjQyOTMwZDFmMTYyOTdlOTQ2MDZmODhlMmU2ZGI=ZGE1ZmI1ZDU4ZDY1YTgzYjUzMWFjMzczNjU5ZjRkNGY=', '0000-00-00 00:00:00'),
-(12, 1, '2017-08-31 15:42:20', 'NzhlZGMyZDRkZGU1YWQwNzk0M2U5NDU1ZjhjZGY4YjY=MGQ4ZjQyOTMwZDFmMTYyOTdlOTQ2MDZmODhlMmU2ZGI=NzI3ZjcxOWFhMTYyYjg4OTYxNzliMDE4Nzc1YTk0MGI=', '2017-09-02 17:42:20'),
-(13, 1, '2017-08-31 15:48:40', 'ZDNhOGFiY2NmYzY1OTUzODM4NTYxNjVkY2QzY2NkYTU=MGQ4ZjQyOTMwZDFmMTYyOTdlOTQ2MDZmODhlMmU2ZGI=OTM1NjAyM2NjYmJiNjUyYmE3YzI1MzljNTEwZDg5YTA=', '2017-09-02 21:18:40'),
-(14, 1, '2017-08-31 15:57:30', 'NjcxZTRhYzcxNTlmOTE4NjVjNDQ5Zjk3YjUzZjRlOTU=MGQ4ZjQyOTMwZDFmMTYyOTdlOTQ2MDZmODhlMmU2ZGI=NzEzYmQ0YzVkNmU2YTk3MzQ5MTEzNGYyNzdhNTg5YTU=', '2017-09-02 21:27:30'),
-(15, 1, '2017-08-31 19:37:17', 'MTFkY2Y1NGU0MWQ5ODQ1NjE1NzdkM2ExNjQxYmIyOWM=MGQ4ZjQyOTMwZDFmMTYyOTdlOTQ2MDZmODhlMmU2ZGI=MjUyNmRlMzNkZTBjZjMxOTdiYWJhMWZhNTcyZjViYzc=', '2017-08-31 21:27:59'),
-(16, 1, '2017-08-31 18:27:42', 'NmYyYmVlMzgxMjQ3NmY3NDUyMDk1YTA1OTA5N2Y5YzU=MGQ4ZjQyOTMwZDFmMTYyOTdlOTQ2MDZmODhlMmU2ZGI=ZTdiNDFiOGY0MDZiMjkxOTJjMjc4NDE3NjEyNTg2ZGQ=', '2017-08-31 22:35:41'),
-(17, 1, '2017-08-31 18:38:22', 'NzJhMjQzNWVkZDVlYWYwMWEwMzdiM2M5ZmU2YWVlN2Q=MGQ4ZjQyOTMwZDFmMTYyOTdlOTQ2MDZmODhlMmU2ZGI=ZGRmZDYyZWEyNTNkOGQwNzhlNjlhYzViZWQxMmQ1YzU=', '2017-09-03 00:08:22'),
-(18, 1, '2017-08-31 18:39:30', 'OTA2YWQyZDkxZGMwNmE4YmRmNmJlMjFkNjY3OTgzNGI=MGQ4ZjQyOTMwZDFmMTYyOTdlOTQ2MDZmODhlMmU2ZGI=NWU1NThmN2RlZjdkOWEyZGIwMWUyOTQ3ZmY1ZjEyMjU=', '2017-09-03 00:09:30'),
-(19, 1, '2017-08-31 18:40:10', 'NzAwMGUyNjAyYzI0OTZiMGNkMTViYTE4MTA5Y2QyOGI=MGQ4ZjQyOTMwZDFmMTYyOTdlOTQ2MDZmODhlMmU2ZGI=MDY0NTI1YjBlNjMwMTY4NWJiZGFiMWY2NjE0NWYxNmI=', '2017-09-03 00:10:10'),
-(20, 1, '2017-08-31 18:41:00', 'ZjI0ZWFiODg1MzI1ZTVkZDViNjE3MThhYTU2YzUxMDU=MGQ4ZjQyOTMwZDFmMTYyOTdlOTQ2MDZmODhlMmU2ZGI=MTdmYjUyNjIxZWVkNTI1N2Y4Mjk5YjE0NzI0NDZkZTY=', '2017-09-03 00:11:00'),
-(21, 1, '2017-08-31 18:53:58', 'ZjAxMGQ2YTc2OTJjZDg2Y2FjM2YyNDM5NzQ0ZjY1NGE=MGQ4ZjQyOTMwZDFmMTYyOTdlOTQ2MDZmODhlMmU2ZGI=MTQyMGM4ZWRhM2Y3ZDg3OTEwMDcwZWVmYzBlMDQyNzA=', '2017-09-01 00:22:44'),
-(22, 1, '2017-08-31 18:59:30', 'Y2E1OTczOTE0MmFhYzYxZTU3ZjEzZDFiYjU4MjkwZDU=MGQ4ZjQyOTMwZDFmMTYyOTdlOTQ2MDZmODhlMmU2ZGI=MzMwOWQ3Y2U1YTk4YTg4M2RjZTY5ZDQ2NjNiY2Y1ODg=', '2017-09-03 00:29:30'),
-(23, 1, '2017-09-02 03:15:55', 'NTA4NWNjMDI4MDEzOWMyNTE1NzQ2OGFkYmYyZTMxNzE=MGQ4ZjQyOTMwZDFmMTYyOTdlOTQ2MDZmODhlMmU2ZGI=NWQ1MjNhOWZkZjI3NDRmOGIwNDI4NTVjODc0ODk1ZjE=', '2017-09-04 08:45:55'),
-<<<<<<< HEAD
-(24, 1, '2017-10-22 17:34:18', 'NWYxMzA2ZmYzYzNkODExYmY3MjgwOTQyN2QwYTZjNjI=MGQ4ZjQyOTMwZDFmMTYyOTdlOTQ2MDZmODhlMmU2ZGI=ZmU0MzQzOTliMzhhNTY5Y2M0MDBlZGE2YWM1YjVmNzA=', '2017-10-24 23:04:18');
-=======
-(24, 1, '2017-10-22 17:34:18', 'NWYxMzA2ZmYzYzNkODExYmY3MjgwOTQyN2QwYTZjNjI=MGQ4ZjQyOTMwZDFmMTYyOTdlOTQ2MDZmODhlMmU2ZGI=ZmU0MzQzOTliMzhhNTY5Y2M0MDBlZGE2YWM1YjVmNzA=', '2017-10-24 23:04:18'),
-(25, 1, '2017-10-24 17:41:29', 'ZjJmZGFhODg2MDE4ODUwZDI2YjI4M2RkMzAwZjA4MDQ=MGQ4ZjQyOTMwZDFmMTYyOTdlOTQ2MDZmODhlMmU2ZGI=NWIxMjkwMjY1NzRlYzlmZTZkNjg3YzEwYzE1YzY2N2U=', '2017-10-26 23:11:29');
->>>>>>> order
 
 -- --------------------------------------------------------
 
@@ -206,8 +161,6 @@ INSERT INTO `users` (`user_id`, `first_name`, `last_name`, `email`, `mobile`, `d
 -- --------------------------------------------------------
 
 --
-<<<<<<< HEAD
-=======
 -- Table structure for table `user_address`
 --
 
@@ -222,7 +175,6 @@ CREATE TABLE `user_address` (
 -- --------------------------------------------------------
 
 --
->>>>>>> order
 -- Table structure for table `user_credentials`
 --
 
@@ -240,8 +192,6 @@ INSERT INTO `user_credentials` (`user_login_id`, `user_id`, `password`) VALUES
 (1, 1, 'ODFkYzliZGI1MmQwNGRjMjAwMzZkYmQ4MzEzZWQwNTU='),
 (2, 2, 'ODFkYzliZGI1MmQwNGRjMjAwMzZkYmQ4MzEzZWQwNTU=');
 
-<<<<<<< HEAD
-=======
 -- --------------------------------------------------------
 
 --
@@ -258,6 +208,16 @@ CREATE TABLE `vendors` (
   `location_code` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `vendors`
+--
+
+INSERT INTO `vendors` (`vendor_id`, `vendor_code`, `vendor_name`, `address`, `contact_mobile`, `contact_email`, `location_code`) VALUES
+(1, 'VN001', 'FB Cakes', 'Karapakkam', '9876543210', '123@gmail.com', 'LN004'),
+(2, 'VN002', 'Cake House', 'Thiruvanmiyur', '1234567890', '234@gmail.com', 'LN001'),
+(3, 'VN003', 'Yum Cakes', 'Thoraipakkam', '8765432190', '789@gmail.com', 'LN003'),
+(4, 'VN004', 'Bakers Code', 'Perungudi', '765421098', '667@gmail.com', 'LN002');
+
 -- --------------------------------------------------------
 
 --
@@ -268,28 +228,79 @@ CREATE TABLE `vendor_items` (
   `vendor_item_id` int(11) NOT NULL,
   `vendor_code` varchar(10) NOT NULL,
   `item_code` varchar(10) NOT NULL,
-  `status` int(1) NOT NULL
+  `status` int(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
->>>>>>> order
+--
+-- Dumping data for table `vendor_items`
+--
+
+INSERT INTO `vendor_items` (`vendor_item_id`, `vendor_code`, `item_code`, `status`) VALUES
+(1, 'VN001', 'IT001', 1),
+(2, 'VN001', 'IT002', 1),
+(3, 'VN001', 'IT003', 0),
+(4, 'VN001', 'IT004', 1),
+(5, 'VN001', 'IT005', 0),
+(6, 'VN001', 'IT006', 1),
+(7, 'VN001', 'IT007', 1),
+(8, 'VN002', 'IT001', 1),
+(9, 'VN002', 'IT002', 1),
+(10, 'VN002', 'IT003', 1),
+(11, 'VN002', 'IT004', 1),
+(12, 'VN002', 'IT005', 1),
+(13, 'VN002', 'IT006', 1),
+(14, 'VN002', 'IT007', 1),
+(15, 'VN003', 'IT001', 1),
+(16, 'VN003', 'IT002', 1),
+(17, 'VN003', 'IT003', 0),
+(18, 'VN003', 'IT004', 1),
+(19, 'VN003', 'IT005', 1),
+(20, 'VN003', 'IT006', 1),
+(21, 'VN003', 'IT007', 1),
+(22, 'VN004', 'IT001', 1),
+(23, 'VN004', 'IT002', 1),
+(24, 'VN004', 'IT003', 1),
+(25, 'VN004', 'IT004', 1),
+(26, 'VN004', 'IT005', 0),
+(27, 'VN004', 'IT006', 1),
+(28, 'VN004', 'IT007', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `vendor_items_view`
+-- (See below for the actual view)
+--
+CREATE TABLE `vendor_items_view` (
+`item_code` varchar(10)
+,`item_name` varchar(50)
+,`item_category` varchar(50)
+,`item_price` varchar(10)
+,`qty_slab` int(11)
+,`location_code` varchar(10)
+,`vendor_code` varchar(10)
+,`status` int(1)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `vendor_items_view`
+--
+DROP TABLE IF EXISTS `vendor_items_view`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vendor_items_view`  AS  select `item`.`item_code` AS `item_code`,`item`.`item_name` AS `item_name`,`item`.`item_category` AS `item_category`,`item`.`item_price` AS `item_price`,`item`.`qty_slab` AS `qty_slab`,`vendor`.`location_code` AS `location_code`,`vendor`.`vendor_code` AS `vendor_code`,`vendor_item`.`status` AS `status` from ((`items` `item` join `vendors` `vendor`) join `vendor_items` `vendor_item`) where ((`item`.`item_code` = `vendor_item`.`item_code`) and (`vendor`.`vendor_code` = `vendor_item`.`vendor_code`)) order by `item`.`item_code`,`vendor`.`location_code` ;
+
 --
 -- Indexes for dumped tables
 --
 
 --
-<<<<<<< HEAD
--- Indexes for table `cakes`
+-- Indexes for table `items`
 --
-ALTER TABLE `cakes`
-  ADD PRIMARY KEY (`cake_id`);
-=======
--- Indexes for table `Items`
---
-ALTER TABLE `Items`
+ALTER TABLE `items`
   ADD PRIMARY KEY (`item_id`),
-  ADD UNIQUE KEY `item_code` (`item_code`),
-  ADD KEY `item_vendor` (`vendor_code`);
->>>>>>> order
+  ADD UNIQUE KEY `item_code` (`item_code`);
 
 --
 -- Indexes for table `locations`
@@ -299,8 +310,6 @@ ALTER TABLE `locations`
   ADD UNIQUE KEY `location_code` (`location_code`);
 
 --
-<<<<<<< HEAD
-=======
 -- Indexes for table `orders`
 --
 ALTER TABLE `orders`
@@ -324,7 +333,6 @@ ALTER TABLE `order_items`
   ADD KEY `order_item_code` (`item_code`);
 
 --
->>>>>>> order
 -- Indexes for table `tokens`
 --
 ALTER TABLE `tokens`
@@ -341,15 +349,12 @@ ALTER TABLE `users`
   ADD UNIQUE KEY `mobile` (`mobile`);
 
 --
-<<<<<<< HEAD
-=======
 -- Indexes for table `user_address`
 --
 ALTER TABLE `user_address`
   ADD PRIMARY KEY (`user_address_id`);
 
 --
->>>>>>> order
 -- Indexes for table `user_credentials`
 --
 ALTER TABLE `user_credentials`
@@ -357,8 +362,6 @@ ALTER TABLE `user_credentials`
   ADD KEY `user_id` (`user_id`);
 
 --
-<<<<<<< HEAD
-=======
 -- Indexes for table `vendors`
 --
 ALTER TABLE `vendors`
@@ -375,34 +378,20 @@ ALTER TABLE `vendor_items`
   ADD KEY `vendor_item_status_2` (`item_code`);
 
 --
->>>>>>> order
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
-<<<<<<< HEAD
--- AUTO_INCREMENT for table `cakes`
+-- AUTO_INCREMENT for table `items`
 --
-ALTER TABLE `cakes`
-  MODIFY `cake_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-=======
--- AUTO_INCREMENT for table `Items`
---
-ALTER TABLE `Items`
-  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT;
->>>>>>> order
+ALTER TABLE `items`
+  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT for table `locations`
 --
 ALTER TABLE `locations`
-  MODIFY `location_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `location_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
-<<<<<<< HEAD
--- AUTO_INCREMENT for table `tokens`
---
-ALTER TABLE `tokens`
-  MODIFY `token_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
-=======
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
@@ -421,51 +410,35 @@ ALTER TABLE `order_items`
 -- AUTO_INCREMENT for table `tokens`
 --
 ALTER TABLE `tokens`
-  MODIFY `token_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
->>>>>>> order
+  MODIFY `token_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-<<<<<<< HEAD
   MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
-=======
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 --
 -- AUTO_INCREMENT for table `user_address`
 --
 ALTER TABLE `user_address`
   MODIFY `user_address_id` int(11) NOT NULL AUTO_INCREMENT;
->>>>>>> order
 --
 -- AUTO_INCREMENT for table `user_credentials`
 --
 ALTER TABLE `user_credentials`
   MODIFY `user_login_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
-<<<<<<< HEAD
-=======
 -- AUTO_INCREMENT for table `vendors`
 --
 ALTER TABLE `vendors`
-  MODIFY `vendor_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `vendor_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `vendor_items`
 --
 ALTER TABLE `vendor_items`
-  MODIFY `vendor_item_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `vendor_item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 --
->>>>>>> order
 -- Constraints for dumped tables
 --
-
---
-<<<<<<< HEAD
-=======
--- Constraints for table `Items`
---
-ALTER TABLE `Items`
-  ADD CONSTRAINT `item_vendor` FOREIGN KEY (`vendor_code`) REFERENCES `vendors` (`vendor_code`);
 
 --
 -- Constraints for table `orders`
@@ -488,7 +461,6 @@ ALTER TABLE `order_items`
   ADD CONSTRAINT `order_num_items` FOREIGN KEY (`order_number`) REFERENCES `orders` (`order_number`);
 
 --
->>>>>>> order
 -- Constraints for table `tokens`
 --
 ALTER TABLE `tokens`
@@ -500,8 +472,6 @@ ALTER TABLE `tokens`
 ALTER TABLE `user_credentials`
   ADD CONSTRAINT `user_credentials_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 
-<<<<<<< HEAD
-=======
 --
 -- Constraints for table `vendors`
 --
@@ -515,7 +485,6 @@ ALTER TABLE `vendor_items`
   ADD CONSTRAINT `vendor_item_status_1` FOREIGN KEY (`vendor_code`) REFERENCES `vendors` (`vendor_code`),
   ADD CONSTRAINT `vendor_item_status_2` FOREIGN KEY (`item_code`) REFERENCES `Items` (`item_code`);
 
->>>>>>> order
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
