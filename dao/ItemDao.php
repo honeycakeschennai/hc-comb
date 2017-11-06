c688a3be56209b61bb826efbf6069e691ca42364
Ramu Ramasamy <rram.ramasamy@gmail.com>
2017-11-06 08:30:10 +0530

    Item fetch SQL modified due to business logic mismatch

--- dao/ItemDao.php
+++ dao/ItemDao.php
@@ -27,7 +27,8 @@
 		 */
 		public function getItems($data){
 			$this->log->info(__FUNCTION__ . SPACE . METHOD_STARTS);
-			$query = 'SELECT * FROM ' . VENDOR_ITEMS . ' WHERE location_code="' . $data['locationCode'] . '"';
+			//SELECT items_view.item_code, items_view.item_name, items_view.item_category, items_view.item_price, items_view.qty_slab, items_view.vendor_code, items_view.location_code, items_view.status FROM vendor_items_view items_view, locations loc WHERE loc.delivery_vendor=items_view.vendor_code AND loc.location_code='LN005'
+			$query = 'SELECT items_view.item_code, items_view.item_name, items_view.item_category, items_view.item_price, items_view.qty_slab, items_view.vendor_code, items_view.location_code, items_view.status FROM ' . VENDOR_ITEMS_VIEW . ' items_view, ' . LOCATIONS . ' loc WHERE loc.delivery_vendor=items_view.vendor_code AND loc.location_code="' . $data['locationCode'] . '"';
 			$db = $this->db;
 			$resultMap = $db->selectOperation($query);
 			$this->closeConnection();		
