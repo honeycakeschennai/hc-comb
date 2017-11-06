6ab452ae4c99b9d8fbfe3d583e6c7e1ba3a1aab9
Ramu Ramasamy <rram.ramasamy@gmail.com>
2017-11-06 08:29:44 +0530

    view name changed in script

--- utils/TableNameConstants.php
+++ utils/TableNameConstants.php
@@ -5,6 +5,6 @@
 	define('TOKENS', 'tokens');
 	define('LOCATIONS', 'locations');
 	define('ITEMS', 'items');
-	define('VENDOR_ITEMS', 'vendor_items_view');
+	define('VENDOR_ITEMS_VIEW', 'vendor_items_view');
 
 ?>
